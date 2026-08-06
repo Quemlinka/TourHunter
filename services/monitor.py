ae17e13d0ff62b7b4aab199156dfffc097d749ff
Quemlinka <145176123+Quemlinka@users.noreply.github.com>
2026-08-06 15:43:44 +0300

    Sprint 2: browser module

--- services/monitor.py
+++ services/monitor.py
@@ -1,13 +1,15 @@
+from browser.browser import open_site
 from parsers.demo_parser import find_best_tour
 
 
 def check_tours():
+
+    print("Запускаю браузер...")
+
+    open_site()
+
     tour = find_best_tour()
 
-    print("====== НАЙДЕН ТУР ======")
-    print(f"Страна: {tour.country}")
-    print(f"Город: {tour.city}")
-    print(f"Отель: {tour.hotel}")
-    print(f"Цена: {tour.price} ₽")
+    print(f"{tour.hotel} {tour.price}")
 
     return tour
